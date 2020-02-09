dc2213556bfc0b14a849f797436d6165c708b6fe
Semen_K <idsemen1992@ya.ru>
2020-02-09 14:04:32 +0300

    Second commit homework

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict'
-let budgetMonth = prompt("Ваш бюджет на месяц?");
-let date = prompt("Введите дату в формате YYYY-MM-DD");
-let expens = prompt("Введите обязательную статью расходов в этом месяце");
-let howCost = prompt("Во сколько обойдется?");
+
+let budgetMonth = prompt("Ваш бюджет на месяц?", ''),
+    date = prompt("Введите дату в формате YYYY-MM-DD", '');
+
 let appData = {
     budget : budgetMonth,
     timeData : date,
@@ -11,6 +11,15 @@ let appData = {
     income : [],
     savings : false
 };
-let budgetOfDay = budgetMonth / 30;
-alert('Ваш бюджет на месяц составаит - ' + budgetOfDay);
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert('Ваш бюджет на месяц составаит - ' + appData.budget / 30);
+
 console.log(budgetOfDay);
